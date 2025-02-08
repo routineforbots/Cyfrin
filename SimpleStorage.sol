342b5b3e8db2a29b87415e5ae7b2abf58f000b31
routineforbots <stan@routineforbots.com>
2025-02-09 00:15:18 +0100

    Lesson 8. Arrays and structs - done

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -23,7 +23,7 @@ contract SimpleStorage {
     bytes32 favouriteBytes32 = "cat"; //string objects are actually bytes objects for text
     //bytes and bytesNUMBER (with number specified) are actually different object
 
-    uint256 public favouriteNumber; //has default value = 0; any type of object has default value: boolean is false, etc...
+    uint256 public myFavouriteNumber; //has default value = 0; any type of object has default value: boolean is false, etc...
     //visibility of the variable is set to internal by default which can be changed with public keyword
     //internal - only visible internally, for child contracts and inherited contracts
     //public - visible externaly & internally, creates getter function for storage/state variables
@@ -33,18 +33,49 @@ contract SimpleStorage {
     
     //The following function will update our favouriteNumber; parameters which we will pass to the functions are defined inside ()
     function store(uint256 _favoriteNumber) public { //name of the variables in parameters must be different
-        favouriteNumber = _favoriteNumber;
+        myFavouriteNumber = _favoriteNumber;
     } 
 
     //Every time when we update state of the blockchain - it costs gas. 
     //To not send transaction but just a call (not use gas) we can use view & pure
     //Keyword view - function will only read state from the blockchain
     function retrieve() public view returns(uint256){
-        return favouriteNumber;
+        return myFavouriteNumber;
     }
     //Function with pure keyword don't allow even reading from blockchain storage (where all variables are stored)
     function retrive_pure() public pure returns(uint256) {
         return 7;
     }
+
+    //The array
+    uint256[] mylist;
+    //  0.  1.  2.  - indexes
+    // [77, 78, 90]
+
+    //With the struct keyword you can create your own type
+    struct Person{
+        uint256 favouriteNumber;
+        string name;
+    }
+
+    //creating a variable of custom type Person
+    Person public pat = Person(7, "Pat"); // another form of declaration = Person({favouriteNumber: 7, name: "Pat"})
+
+    //let's create a list/array of persons, Person[] - declaration of type of variable
+    Person[] public listOfPeople; // default value of this list will be empty list 
+    //Person[] - is a dynamic array which grows and shrinks based on amount of elements
+    //Person[3] - static array with size for 3 elements
+
+    //Function to add people to the array 
+    //memory keyword means a location of the data
+    function addPeople(string memory _name, uint256 _favouriteNumber) public {
+        //one of the examples how to add a new element of the array:
+        //Person memory newPerson = Person(_favouriteNumber, _name);
+        //listOfPeople.push(newPerson);
+
+        //But better way to define it is the following:
+        listOfPeople.push(Person(_favouriteNumber, _name));
+    }
+
 }
 
