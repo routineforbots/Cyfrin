60f8f6e1ded3f1b502a092802f233e8f8f56df39
routineforbots <stan@routineforbots.com>
2025-02-08 20:50:50 +0100

    Lesson 7 - Functions is done

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -23,9 +23,28 @@ contract SimpleStorage {
     bytes32 favouriteBytes32 = "cat"; //string objects are actually bytes objects for text
     //bytes and bytesNUMBER (with number specified) are actually different object
 
+    uint256 public favouriteNumber; //has default value = 0; any type of object has default value: boolean is false, etc...
+    //visibility of the variable is set to internal by default which can be changed with public keyword
+    //internal - only visible internally, for child contracts and inherited contracts
+    //public - visible externaly & internally, creates getter function for storage/state variables
+    //private - only visible in the current contract
+    //external - for functions only, can only be message-called via this.func
 
-    uint256 favouriteNumber; //has default value = 0; any type of object has default value: boolean is false, etc...
     
+    //The following function will update our favouriteNumber; parameters which we will pass to the functions are defined inside ()
+    function store(uint256 _favoriteNumber) public { //name of the variables in parameters must be different
+        favouriteNumber = _favoriteNumber;
+    } 
 
+    //Every time when we update state of the blockchain - it costs gas. 
+    //To not send transaction but just a call (not use gas) we can use view & pure
+    //Keyword view - function will only read state from the blockchain
+    function retrieve() public view returns(uint256){
+        return favouriteNumber;
+    }
+    //Function with pure keyword don't allow even reading from blockchain storage (where all variables are stored)
+    function retrive_pure() public pure returns(uint256) {
+        return 7;
+    }
 }
 
