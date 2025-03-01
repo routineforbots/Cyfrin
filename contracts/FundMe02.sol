abb515b484ad47d6364679d80f4f416069b5ecbf
routineforbots <stan@routineforbots.com>
2025-03-01 20:15:51 +0100

    15. Creating your own libraries

---
+++ contracts/FundMe02.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {PriceConverter} from "./PriceConverter.sol";
+
+contract FundMe {
+    // this is how we attach library to specific data type
+    using PriceConverter for uint256; 
+    // because function has uint256 as a parameter: getConvertionRate(uint256 _ethAmount)
+
+    uint256 public myValue = 10;
+    uint256 public minUSD = 5 * (10 ** 18); 
+    address[] public funders;
+    mapping(address funder => uint256 amountFunded ) public addressToAmountFunded;
+
+
+
+function fund() public payable {
+    // require(getConvertionRate(msg.value) >= minUSD, "didn't send enough ETH");
+    require(msg.value.getConvertionRate() >= minUSD, "didn't send enough ETH");
+    // if getConvertionRate will have 2 parameters getConvertionRate(uint256 _ethAmount, uint256 _someThing)
+    // then we will call it like that: msg.value.getConvertionRate(123), where msg.value is the 1st parameter and 123 is the 2nd
+    
+    funders.push(msg.sender);
+    addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value; 
+}
+
+
+
+
+// withdraw funds back to the owner of the conract
+function withdraw() public {
+
+}
+
+
+
+
+}
---
+++ contracts/PriceConverter.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol"; 
+
+// for library we need to change visiability of all functions from public -> internal
+
+library PriceConverter {
+   
+    function getPrice() internal view returns (uint256) {
+        AggregatorV3Interface pricefeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (, int answer, , ,) = pricefeed.latestRoundData(); 
+        return uint256(answer * 1e10);
+    }
+
+    function getConvertionRate(uint256 _ethAmount) internal view returns(uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (_ethAmount * ethPrice) / 1e18; 
+        return ethAmountInUsd;
+
+    }
+
+    function getVersion() internal view returns (uint256) {
+        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+    }
+}
