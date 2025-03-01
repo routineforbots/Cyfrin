9fde49e70095e2e4afdc03af17b1d8a9e476c962
routineforbots <stan@routineforbots.com>
2025-03-01 22:40:56 +0100

    19. Resetting an Array

--- contracts/FundMe02.sol
+++ contracts/FundMe02.sol
@@ -20,16 +20,32 @@ function fund() public payable {
     require(msg.value.getConvertionRate() >= minUSD, "didn't send enough ETH");
     // if getConvertionRate will have 2 parameters getConvertionRate(uint256 _ethAmount, uint256 _someThing)
     // then we will call it like that: msg.value.getConvertionRate(123), where msg.value is the 1st parameter and 123 is the 2nd
-    
+
     funders.push(msg.sender);
-    addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value; 
+    // addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value; 
+    addressToAmountFunded[msg.sender] += msg.value; //alternative version of the above code
 }
-
-
 
 
 // withdraw funds back to the owner of the conract
 function withdraw() public {
+    /*
+    for loop
+    [1, 2, 3, 4] - elements
+     0, 1, 2, 3  - indexes
+    */
+    for (/* starting index, ending index, step amount */ uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+        address funderAddress = funders[funderIndex];
+        addressToAmountFunded[funderAddress] = 0;
+    }
+
+    // we will reset the array by creating new one
+    funders = new address[](0); // (0) - means starting length equals zero
+
+
+    // withdraw the funds
+    
+
 
 }
 
