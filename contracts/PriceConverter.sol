c6f834036d11dbfdf921a2099e80a40fa63abc55
routineforbots <stan@routineforbots.com>
2025-03-03 00:34:25 +0100

    minors

--- contracts/PriceConverter.sol
+++ contracts/PriceConverter.sol
@@ -8,7 +8,11 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interf
 library PriceConverter {
    
     function getPrice() internal view returns (uint256) {
-        AggregatorV3Interface pricefeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        // AggregatorV3Interface pricefeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+
+        // different address is used for ZKSync
+        AggregatorV3Interface pricefeed = AggregatorV3Interface(0xfEefF7c3fB57d18C5C6Cdd71e45D2D0b4F9377bF);
+
         (, int answer, , ,) = pricefeed.latestRoundData(); 
         return uint256(answer * 1e10);
     }
